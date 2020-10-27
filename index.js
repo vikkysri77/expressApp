36920b6dda73d721a3621f5ed48046e4e02fabc4
sridhar_gopu <vikkysri77@gmail.com>
2020-10-27 22:35:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+var fs = require('fs');
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+
+// create application/json parser
+var jsonParser = bodyParser.json()
+ 
+app.listen(3000, () => {
+ console.log("Server running on port 3000");
+});
+
+app.get("/results", (req, res, next) => {
+    res.json("Application is up");
+   });
+
+
+  app.post('/protractor/scripts', jsonParser, function (req, res) {
+    // var files = fs.readdirSync('/assets/photos/');
+
+    if(req.body.scriptName !== undefined && req.body.scriptName !== null && req.body.scriptName !== ""){
+        console.log(req.body.scriptName);
+        fs.readdir('./', (err, files) => {
+            files.forEach(file => {
+              console.log(file);
+            });
+          });
+        res.send("Hello");
+    } else  {
+        res.status(404).json({ error: 'something is wrong' });
+    };
+
+  });
